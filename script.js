5a49310b7dbe3d14751cdc08e7457300f9eedf1a
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-01-16 14:25:56 +0100

    feat: implement leaflet script to display the map

--- script.js
+++ script.js
@@ -16,8 +16,21 @@ if (navigator.geolocation)
     function (position) {
       console.log(position);
       const { latitude, longitude } = position.coords;
-      console.log('latitude', latitude, 'longitude', longitude);
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      // console.log('latitude', latitude, 'longitude', longitude);
+      // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
